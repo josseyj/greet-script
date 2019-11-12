5e696ddebbe0f415a4bce3aebe2d066c18c0d602
Jossey Jacob <josseyj@Josseys-MacBook-Pro.local>
2019-11-12 07:05:42 -0500

    Initial commit.

---
+++ greet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+greet() {
+	h=`date +%H`
+	if [ $h -lt 12 ]; then
+	  echo Good morning
+	elif [ $h -lt 18 ]; then
+	  echo Good afternoon
+	else
+	  echo Good evening
+	fi
+}
+
+greet;
